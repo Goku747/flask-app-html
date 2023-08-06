d3ff7252489571dbf128f2c15c89566038621c1b
gk <rgoku747@gmail.com>
2023-08-06 15:00:19 +0530

    Added html

--- flask-app-web.py
+++ flask-app-web.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
+JOBS = [
+    {'Id':1, 'Title': 'Data Analyst', 'Location': 'Chennai', 'Salary': Rs: 20L },
+    {'Id':2, 'Title': 'Data Scientist', 'Location': 'Bangalore', 'Salary': Rs: 25L }
+    ]
 @app.route('/')
 def careers_home():
-    return "This is a careers page!"
+    return render_template('home.html', jobs=JOBS)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
