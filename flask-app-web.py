ddb5c42a02b11d66bfcf15eef958f400e540f5e3
gk <rgoku747@gmail.com>
2023-08-06 23:51:33 +0530

    Added jobs and apply button

--- flask-app-web.py
+++ flask-app-web.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 JOBS = [
-    {'Id':1, 'Title': 'Data Analyst', 'Location': 'Chennai', 'Salary': Rs: 20L },
-    {'Id':2, 'Title': 'Data Scientist', 'Location': 'Bangalore', 'Salary': Rs: 25L }
-    ]
+    { 'Id':1, 'Title': 'Data Analyst', 'Location': 'Chennai', 'Experience': '0-1yr' },
+    { 'Id':2, 'Title': 'Data Scientist', 'Location': 'Bangalore', 'Experience': '5-8yrs' }
+]
 @app.route('/')
 def careers_home():
     return render_template('home.html', jobs=JOBS)
-
+@app.route('/api/jobs')
+def job_list():
+    return jsonify(JOBS)
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
